8e306e917f50e94584e9b89acce1acea9209e174
JDCar1241 <jcardenasv3.a@upao.edu.pe>
2026-07-18 09:31:16 -0500

    Feat : agragacion de clase pedido

--- src/cafelinea/Pedido.java
+++ src/cafelinea/Pedido.java
@@ -4,10 +4,25 @@
  */
 package cafelinea;
 
-/**
- *
- * @author sistemas
- */
 public class Pedido {
-    
+
+    private int numeroPedido;
+    private String cliente;
+    private Cafe cafe;
+
+    public Pedido(int numeroPedido, String cliente, Cafe cafe) {
+        this.numeroPedido = numeroPedido;
+        this.cliente = cliente;
+        this.cafe = cafe;
+    }
+
+    public void mostrarPedido() {
+
+        System.out.println("\n========== PEDIDO ==========");
+        System.out.println("Número: " + numeroPedido);
+        System.out.println("Cliente: " + cliente);
+        System.out.println("Producto: " + cafe.getDescripcion());
+        System.out.println("Total: S/ " + cafe.getPrecio());
+        System.out.println("============================");
+    }
 }
